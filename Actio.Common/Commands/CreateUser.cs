35038bd173e9d1b40371844332577fa4255641e3
Khwezi Mngoma <khwezi@mngoma.co.za>
2019-03-30 20:28:27 +0200

    Actio.Common: added basic commands and implemented first half of the command handler pattern

---
+++ Actio.Common/Commands/CreateUser.cs
@@ -0,0 +1,11 @@
+﻿namespace Actio.Common.Commands
+{
+    public class CreateUser : ICommand
+    {
+        public string Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Email { get; set; }
+    }
+}
