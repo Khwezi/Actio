440081ee236845a73d7b6ebc292f0b9d1c6c4749
Khwezi Mngoma <khwezi@mngoma.co.za>
2019-03-31 08:16:16 +0200

    Actio.Common.Extensions: progress push

---
+++ Actio.Common/RabbitMq/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Actio.Common.Commands;
+using RawRabbit;
+
+namespace Actio.Common.RabbitMq
+{
+    public static class Extensions
+    {
+        public static async Task WithCommandHandlerAsync<TCommand>(this IBusClient bus,
+            ICommandHandler<TCommand> handler) where TCommand : ICommand
+            => bus.SubscribeAsync<TCommand>(msg => handler.HandleAsync(msg),
+               ctx => ctx.UseConsumerConfiguration(cfg => cfg.FromDelaredQueue(q => q.WithName("udemy"));
+    }
+}
